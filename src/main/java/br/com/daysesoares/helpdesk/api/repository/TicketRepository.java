86bbadc2aabeedf1869ea7170edbe8ce7913fe48
daysess <daysedesign@gmail.com>
2021-09-02 10:28:40 -0300

    Configuracao do application.properties

--- src/main/java/br/com/daysesoares/helpdesk/api/repository/TicketRepository.java
+++ src/main/java/br/com/daysesoares/helpdesk/api/repository/TicketRepository.java
@@ -10,7 +10,7 @@ import br.com.daysesoares.helpdesk.api.entity.Ticket;
 public interface TicketRepository extends MongoRepository<Ticket, String>{
 
 	Page<Ticket> findByUserIdOrderByDateDesc(String userId, Pageable pages);
-	
+	/*
 	Page<Ticket> findByTitleIgnoreCaseContainingAndStatusAndPriorityOrderByDateDesc(
 			String title, String status, String Priority, Pageable pages);
 	
@@ -19,7 +19,7 @@ public interface TicketRepository extends MongoRepository<Ticket, String>{
 	
 	Page<Ticket> findByTitleIgnoreCaseContainingAndStatusAndPriorityAssignedUserIdOrderByDateDesc(
 			String title, String status, String Priority, Pageable pages);
-	
+	*/
 	Page<Ticket> findByNumber(Integer number, Pageable pages);
 	
 }
